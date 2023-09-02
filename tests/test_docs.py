3e8cd5db9deb49c31f2fbc730e5af4b19d1fe8d1
Nicholas Felt <nicholas.felt@tektronix.com>
2023-09-02 10:51:01 -0700

    test: Update doctests to use sphinx build instead of make.

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -6,11 +6,29 @@ import subprocess
 import sys
 
 from importlib.util import find_spec
-from typing import Generator
+from typing import Generator, List
 
 import pytest
 
 from conftest import PROJECT_ROOT_DIR
+
+
+def create_sphinx_build_cmd(build_type: str) -> List[str]:
+    """Create the sphinx-build command to send via subprocess.
+
+    Args:
+        build_type: The type of sphinx build to perform.
+
+    Returns:
+        The sphinx build command
+    """
+    source_dir = "."
+    build_dir = "_build"
+    num_processors = os.getenv("SPHINX_PROC_COUNT", "auto")
+    return shlex.split(
+        f'python -msphinx -M {build_type} "{source_dir}" "{build_dir}" '
+        f"-W --keep-going -j {num_processors}"
+    )
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -38,20 +56,20 @@ class TestDocs:  # pylint: disable=no-self-use
     @pytest.mark.order(1)
     def test_docs_html(self) -> None:
         """Test creating html documentation."""
-        subprocess.check_call(shlex.split("make html"))  # noqa: S603
+        subprocess.check_call(create_sphinx_build_cmd("html"))  # noqa: S603
 
     @pytest.mark.xfail(reason="tm_devices GitHub links don't work currently")
     @pytest.mark.order(2)
     def test_docs_linkcheck(self) -> None:
         """Run the linkcheck test for the documentation."""
-        subprocess.check_call(shlex.split("make linkcheck"))  # noqa: S603
+        subprocess.check_call(create_sphinx_build_cmd("linkcheck"))  # noqa: S603
 
     @pytest.mark.order(3)
     def test_docs_coverage(self) -> None:
         """Run the coverage test for the documentation."""
-        subprocess.check_call(shlex.split("make coverage"))  # noqa: S603
+        subprocess.check_call(create_sphinx_build_cmd("coverage"))  # noqa: S603
 
     @pytest.mark.order(4)
     def test_docs_doctest(self) -> None:
         """Run the doctest test for the documentation."""
-        subprocess.check_call(shlex.split("make doctest"))  # noqa: S603
+        subprocess.check_call(create_sphinx_build_cmd("doctest"))  # noqa: S603
