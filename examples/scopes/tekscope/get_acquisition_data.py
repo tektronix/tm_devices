9c796fdd446849e925db611ce22d2b83c4bf9a11
Nicholas Felt <nicholas.felt@gmail.com>
2023-11-06 11:08:51 -0800

    fix: Update a few comments to have better wording.

--- examples/scopes/tekscope/get_acquisition_data.py
+++ examples/scopes/tekscope/get_acquisition_data.py
@@ -14,7 +14,7 @@ with DeviceManager(verbose=True) as device_manager:
     # Creating Scope driver object by providing ip address.
     scope: MSO6B = device_manager.add_scope("127.0.0.1")  # pyright: ignore[reportGeneralTypeIssues]
 
-    # Make channel 1 ON
+    # Turn on channel 1
     scope.commands.display.waveview1.ch[1].state.write("ON")
 
     # Set channel 1 vertical scale to 10mV
@@ -26,7 +26,7 @@ with DeviceManager(verbose=True) as device_manager:
     # Set horizontal position to 100
     scope.commands.horizontal.position.write(10)
 
-    #  Set trigger type to Edge
+    # Set trigger type to Edge
     scope.commands.trigger.a.type.write("EDGE")
 
     # Acquisition setup
@@ -40,7 +40,7 @@ with DeviceManager(verbose=True) as device_manager:
     scope.commands.measurement.meas[1].source.write("CH1")
     scope.commands.measurement.meas[2].source.write("CH1")
 
-    # Get the measurements values
+    # Get the measurement values
     scope.commands.acquire.state.write("ON")
 
     if int(scope.commands.opc.query()) == 1:
