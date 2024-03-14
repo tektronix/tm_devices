4aa89b0309a1f64713377bb971c2ca0141d5c043
Ganesh V <ganesh.v@tektronix.com>
2024-03-14 17:58:46 +0530

    fix: Measurement source selection API added to DPO4k, MDO3K models.
    Signed-off-by: v12ganesh ganesh.v@tektronix.com

--- src/tm_devices/commands/_mdo4k_commands.py
+++ src/tm_devices/commands/_mdo4k_commands.py
@@ -11,7 +11,6 @@ from typing import Any, Dict, Optional
 
 from tm_devices.drivers.pi.pi_device import PIDevice
 
-from ._1kjd62_mdo.measurement import Measurement
 from ._1kjd62_mdo.rf import Rf
 from ._1la1ym_msomdodpo.trigger import Trigger
 from ._1lcv3a_msodpomdo.message import Message
@@ -59,6 +58,7 @@ from ._1mlt9u_mdomsodpo.configuration import Configuration
 from ._1mlt9u_mdomsodpo.deskew import Deskew
 from ._1mlt9u_mdomsodpo.display import Display
 from ._1mlt9u_mdomsodpo.mask import Mask
+from ._1mlt9u_mdomsodpo.measurement import Measurement
 from ._1mlt9u_mdomsodpo.recall import Recall
 from ._1mlt9u_mdomsodpo.select import Select
 from ._1nmc1o_msodpomdo.clearmenu import Clearmenu
