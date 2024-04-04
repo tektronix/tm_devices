d597e0ab7ed1af874e99b2196bae67dd7ee03ed5
Nicholas Felt <nicholas.felt@tektronix.com>
2024-04-04 16:58:08 -0700

    docs: Update pre and post processing functionality to not cause autoapi to crash when building docs.
    Signed-off-by: Nicholas Felt <nicholas.felt@tektronix.com>

--- docs/conf.py
+++ docs/conf.py
@@ -9,10 +9,9 @@ import os
 import re
 import shutil
 
-from dataclasses import dataclass
 from importlib.metadata import metadata
 from pathlib import Path
-from typing import Any, Dict, List, Sequence
+from typing import Any, cast, Dict, List, Sequence, Union
 
 from autoapi.mappers.python.objects import PythonPythonMapper  # type: ignore[import]
 from jinja2.environment import Environment as JinjaEnvironment
@@ -189,29 +188,26 @@ mermaid_params = ["-p", "puppeteer-config.json"]
 
 
 # -- Sphinx setup functions --------------------------------------------------
-@dataclass(frozen=True)
-class GFMFileData:  # noqa: D101
-    original_contents: str
-    search_and_replace_pairs: Dict[str, str]
-
-
 README = Path(__file__).parents[1] / "README.md"
 ORIGINAL_README_CONTENT = README.read_text(encoding="utf-8")
-FILES_WITH_GFM = {
-    README: GFMFileData(
-        ORIGINAL_README_CONTENT,
-        {
+# Note: While a dataclass would be preferred here (instead of nested dictionaries),
+# creating a dataclass caused sphinx-autoapi to encounter a MaximumRecursionError
+# when the build is started. For this reason, a nested dictionary was used.
+FILES_WITH_GFM: Dict[Path, Dict[str, Union[str, Dict[str, str]]]] = {
+    README: {
+        "original_contents": ORIGINAL_README_CONTENT,
+        "search_and_replace_pairs": {
             "Glossary": "{doc}`glossary`",
-            "AFGs": "{term}`AFGs <AFG>`",
-            "AWGs": "{term}`AWGs <AWG>`",
-            "Scopes": "{term}`Scopes <Scope>`",
-            "PSUs": "{term}`PSUs <PSU>`",
-            "SMUs": "{term}`SMUs <SMU>`",
-            "MTs": "{term}`MTs <MT>`",
-            "DMMs": "{term}`DMMs <DMM>`",
-            "DAQs": "{term}`DAQs <DAQ>`",
-            "SSs": "{term}`SSs <SS>`",
-            "DPOJET": "{term}`DPOJET`",
+            " AFGs ": " {term}`AFGs <AFG>` ",
+            " AWGs ": " {term}`AWGs <AWG>` ",
+            " Scopes ": " {term}`Scopes <Scope>` ",
+            " PSUs ": " {term}`PSUs <PSU>` ",
+            " SMUs ": " {term}`SMUs <SMU>` ",
+            " MTs ": " {term}`MTs <MT>` ",
+            " DMMs ": " {term}`DMMs <DMM>` ",
+            " DAQs ": " {term}`DAQs <DAQ>` ",
+            " SSs ": " {term}`SSs <SS>` ",
+            " DPOJET ": " {term}`DPOJET` ",
             " PI ": " {term}`PI` ",
             " TSP ": " {term}`TSP` ",
             " API ": " {term}`API` ",
@@ -219,7 +215,7 @@ FILES_WITH_GFM = {
             " 🚧 ": " {term}`🚧` ",
             " ❌ ": " {term}`❌` ",
         },
-    ),
+    },
 }
 
 
@@ -228,7 +224,7 @@ def pre_process_gfm(app: Sphinx) -> None:
     _ = app
     for file, gfm_file_data in FILES_WITH_GFM.items():
         modified_file_contents = ""
-        for line in gfm_file_data.original_contents.splitlines():
+        for line in cast(str, gfm_file_data["original_contents"]).splitlines():
             new_line = line
             if new_line.startswith("> \\["):
                 new_line = re.sub(r"> \\\[!([A-Z]+)\\]", r":::{\1}", new_line).lower()
@@ -237,8 +233,8 @@ def pre_process_gfm(app: Sphinx) -> None:
 
             modified_file_contents += new_line + "\n"
 
-        for search, replace in gfm_file_data.search_and_replace_pairs.items():
-            modified_file_contents = modified_file_contents.replace(search, replace)
+        for search, replace in gfm_file_data["search_and_replace_pairs"].items():  # pyright: ignore[reportAttributeAccessIssue,reportUnknownMemberType,reportUnknownVariableType]
+            modified_file_contents = modified_file_contents.replace(search, replace)  # pyright: ignore[reportUnknownArgumentType]
 
         file.write_text(modified_file_contents, encoding="utf-8")
 
@@ -247,7 +243,7 @@ def post_process_gfm(app: Sphinx, exception: Any) -> None:
     """Restore GitHub Flavored Markdown files."""
     _ = app, exception
     for file, gfm_file_data in FILES_WITH_GFM.items():
-        file.write_text(gfm_file_data.original_contents, encoding="utf-8")
+        file.write_text(gfm_file_data["original_contents"], encoding="utf-8")  # pyright: ignore[reportArgumentType]
 
 
 def skip_member(
