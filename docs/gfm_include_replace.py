c9f11cb53ebc9b4a0e691824747fb114c0868eb8
Nicholas Felt <nicholas.felt@tektronix.com>
2024-04-05 11:13:29 -0700

    docs: Switched to using a custom directive extension for processing GitHub Flavored Markdown with search and replace capability to eliminate the need to modify file contents on-disk.
    Signed-off-by: Nicholas Felt <nicholas.felt@tektronix.com>

---
+++ docs/gfm_include_replace.py
@@ -0,0 +1,85 @@
+"""An extension for including GitHub Flavored Markdown files with text replacement."""
+
+import re
+
+from typing import cast, List
+
+from docutils import nodes, utils
+from docutils.parsers.rst import Directive, directives
+from myst_parser.parsers.docutils_ import Parser
+from sphinx.application import Sphinx
+from sphinx.environment import BuildEnvironment
+
+
+class GFMIncludeReplaceDirective(Directive):
+    """A directive which includes a file but adds search and replace functionality.
+
+    This directive also processes GitHub Flavored Markdown admonitions into MyST admonitions.
+    """
+
+    required_arguments = 1
+    optional_arguments = 0
+    final_argument_whitespace = True
+    option_spec = {  # noqa: RUF012
+        "replace": directives.unchanged,
+    }
+
+    def run(self) -> List[nodes.Node]:  # pylint: disable=too-many-locals
+        """Run the directive.
+
+        This will include the file contents, then search and replace text as
+        defined in the directive. This directive also processes GitHub Flavored Markdown admonitions
+        into MyST admonitions.
+
+        Returns:
+            All the directive's nodes.
+        """
+        settings = self.state.document.settings
+        # noinspection PyUnresolvedReferences
+        env = cast(BuildEnvironment, self.state.document.settings.env)  # pyright: ignore[reportUnknownMemberType]
+        filename = self.arguments[0]
+        replace_rules_str = self.options.get("replace", "")
+        replace_rules = [rule.split("->") for rule in replace_rules_str.split(";")]
+
+        # Resolve the path relative to the source directory
+        rel_filename, filename = env.relfn2path(filename)
+        env.note_dependency(rel_filename)
+
+        try:
+            with open(filename, encoding="utf-8") as file:
+                original_content = file.read()
+                content = ""
+
+                # Replace GFM annotations with MyST admonitions
+                # - https://github.com/orgs/community/discussions/16925
+                # - https://myst-parser.readthedocs.io/en/latest/syntax/admonitions.html
+                for line in original_content.splitlines():
+                    new_line = line
+                    if new_line.startswith("> \\["):
+                        new_line = re.sub(r"> \\\[!([A-Z]+)\\]", r"```{\1}", new_line).lower()
+                    elif new_line.startswith("> "):
+                        new_line = new_line.lstrip("> ") + "\n```"
+                    content += new_line + "\n"
+
+                # Perform the search and replace
+                for rule in replace_rules:
+                    # Check to make sure both a search item and replace item were provided
+                    if len(rule) == 2:  # noqa: PLR2004
+                        content = re.sub(rule[0], rule[1], content)
+
+            # Use the custom parser specified in the directive options
+            document = utils.new_document(filename, settings)
+            parser = Parser()
+            parser.parse(content, document)
+            # clean up doctree and complete parsing
+            document.transformer.populate_from_components((parser,))
+            document.transformer.apply_transforms()
+            return document.children  # noqa: TRY300
+        except Exception as e:  # noqa: BLE001
+            message = f"Error including file {filename}: {e!s}"
+            raise self.error(message) from e
+
+
+def setup(app: Sphinx) -> None:
+    """Set up the Sphinx application to add the new directive."""
+    app.add_directive("gfm-include-replace", GFMIncludeReplaceDirective)
