d63adeff56408130c5d010da80f7dc9bec65f7aa
Nicholas Felt <nicholas.felt@tektronix.com>
2024-04-10 15:23:03 -0700

    docs: Make a few updates to the sphinx configuration to clean up property display and attempt to fix the doc build memory issues.
    Signed-off-by: Nicholas Felt <nicholas.felt@tektronix.com>

--- docs/conf.py
+++ docs/conf.py
@@ -6,9 +6,13 @@ documentation: https://www.sphinx-doc.org/en/master/usage/configuration.html
 """
 
 import os
+import re
+import shutil
+import sys
 
 from importlib.metadata import metadata
-from typing import Any, List, Sequence
+from pathlib import Path
+from typing import List
 
 from autoapi.mappers.python.objects import PythonPythonMapper  # type: ignore[import]
 from jinja2.environment import Environment as JinjaEnvironment
@@ -16,7 +20,11 @@ from sphinx.application import Sphinx
 from urllib3 import disable_warnings
 from urllib3.exceptions import InsecureRequestWarning
 
+SKIP_API_GEN = os.getenv("SKIP_TM_DEVICES_API_GENERATION") is not None
+
+
 disable_warnings(InsecureRequestWarning)
+sys.path.insert(0, Path(__file__).parent.as_posix())
 
 # -- Project information -----------------------------------------------------
 project = "tm_devices"
@@ -31,13 +39,14 @@ copyright = f"2022, {author}"  # noqa: A001
 # ones.
 extensions = [
     "myst_parser",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    # "sphinx.ext.autosummary",
+    "sphinx_autodoc_typehints",
     "autoapi.extension",
     "sphinx.ext.intersphinx",
-    "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "sphinx.ext.extlinks",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.autodoc.typehints",
     "sphinx.ext.coverage",
     "sphinx.ext.doctest",
     "sphinx.ext.duration",
@@ -50,7 +59,19 @@ extensions = [
     "sphinx_togglebutton",
     "sphinx_copybutton",
     "sphinx_tippy",
+    "notfound.extension",
+    "sphinx_toolbox.github",
+    "sphinx_toolbox.sidebar_links",
+    "sphinx_toolbox.collapse",
+    "sphinx_toolbox.more_autodoc.generic_bases",
+    "sphinx_toolbox.more_autodoc.overloads",
     "gfm_include_replace",  # local extension that enables including a file with text replacement
+]
+if sys.version_info >= (3, 9):
+    extensions.append("sphinx_remove_toctrees")
+remove_from_toctrees = [
+    "_autoapi/tm_devices/*",
+    "_autoapi/tm_devices/**/*",
 ]
 
 # List of patterns, relative to source directory, that match files and
@@ -64,9 +85,8 @@ exclude_patterns = [
 templates_path = ["_templates"]
 tls_verify = False
 nitpicky = False  # TODO: change this to `True`
-# rst_prolog = """
-# .. role:: apisummarylabel
-# """
+github_username = author.lower()
+github_repository = project
 # -- Options for extlinks -----------------------------------------------------
 extlinks_detect_hardcoded_links = True
 extlinks = {
@@ -77,10 +97,36 @@ extlinks = {
 }
 
 
-# -- Options for autoapi/autodoc ---------------------------------------------
-def item_in_sequence(sequence: Sequence[Any], item: Any) -> bool:
-    """Check if an item is in a sequence."""
-    return item in sequence
+# -- Options for autoapi/autodoc/autosummary-----------------------------------
+LINK_OBJS_DELIMS = r"(\s*[\[\]\(\),]\s*)"
+LINK_OBJS_DELIMS_RE = re.compile(LINK_OBJS_DELIMS)
+
+
+def _link_objs(value: str) -> str:
+    """Create a python object link.
+
+    Args:
+        value: The object name to use to create the link.
+
+    Returns:
+        The object link.
+    """
+    result = ""
+
+    sub_targets = re.split(LINK_OBJS_DELIMS, value.strip())
+    for target in sub_targets:
+        sub_target = target.strip()
+        if LINK_OBJS_DELIMS_RE.match(sub_target):
+            result += f"{sub_target}"
+            if sub_target.endswith(","):
+                result += " "
+            else:
+                result += "\\ "
+        elif sub_target:
+            result += f":py:obj:`~{sub_target}`\\ "
+
+    # Strip off the extra "\ "
+    return result[:-2]
 
 
 def prep_jinja_env(jinja_env: JinjaEnvironment) -> None:
@@ -89,49 +135,51 @@ def prep_jinja_env(jinja_env: JinjaEnvironment) -> None:
     Args:
         jinja_env: The Jinja environment.
     """
-    jinja_env.tests["contains"] = item_in_sequence  # pyright: ignore[reportArgumentType,reportUnknownMemberType]
+    jinja_env.filters["link_objs_custom"] = _link_objs  # pyright: ignore[reportUnknownMemberType]
 
 
-# FUTURE: # autoapi_prepare_jinja_env = prep_jinja_env
-# FUTURE: # autoapi_template_dir = "_templates/autoapi"
+autoapi_prepare_jinja_env = prep_jinja_env
+autoapi_template_dir = "_templates/autoapi"
+autoapi_generate_api_docs = True
+autoapi_add_toctree_entry = True
+napoleon_preprocess_types = True
 autoapi_root = "_autoapi"
 autoapi_keep_files = True
 autoapi_dirs = ["../src"]
+autoapi_ignore = [
+    # "*/commands/*",
+]
 autoapi_member_order = "alphabetical"
 autodoc_member_order = "alphabetical"
-autoapi_ignore = [
-    "*migrations*",
-]
-autoapi_modules = {
-    "tm_devices": {
-        "prune": True,
-    }
-}
-autoapi_options = [
-    "members",
-    "undoc-members",
-    "special_members",
-    "show-inheritance",
-    "show-module-summary",
-    "imported-members",
-    # "inherited-members",  # commenting this out significantly reduces documentation build time
-]
+autodoc_inherit_docstrings = True
 autodoc_typehints = "both"
 autodoc_typehints_format = "short"
 autodoc_class_signature = "separated"
 autoapi_python_class_content = "class"
 autoclass_content = "class"
-autoapi_generate_api_docs = True
+# FUTURE # autoapi_own_page_level = "module"  # TODO: investigate after sphinx-autoapi==3.1.x
+autoapi_options = [
+    "members",
+    # "inherited-members",  # commenting this out significantly reduces documentation build time
+    # "undoc-members",  # we don't want to include these
+    # "private-members",  # we don't want to include these
+    # "special-members",  # we don't want to include these
+    "show-inheritance",
+    "show-module-summary",
+    # FUTURE # "imported-members",
+]
 # This requires Graphviz to be installed, https://graphviz.org/
-# FUTURE # if shutil.which("dot"):
-# FUTURE #     autoapi_options.append("show-inheritance-diagram")
+if shutil.which("dot"):
+    autoapi_options.append("show-inheritance-diagram")
 
 # -- Options for HTML output -------------------------------------------------
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+html_show_sourcelink = False
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 html_css_files = ["theme_overrides.css"]
+html_js_files = ["insert-br.js"]
 html_theme_options = {
     "navigation_depth": 4,
     "collapse_navigation": True,
@@ -143,7 +191,16 @@ intersphinx_mapping = {  # pylint: disable=consider-using-namedtuple-or-dataclas
     "packaging": ("https://packaging.pypa.io/en/stable/", None),
     "requests": ("https://docs.python-requests.org/en/latest/", None),
 }
-intersphinx_disabled_reftypes = ["*"]
+notfound_context = {
+    "title": "Page Not Found",
+    "body": """
+<h1>Page Not Found</h1>
+
+<p>Sorry, we couldn't find that page.</p>
+
+<p>Try using the search box or go to the homepage.</p>
+""",
+}
 
 # -- Options for docstring coverage ------------------------------------------
 coverage_show_missing_items = True
@@ -185,8 +242,15 @@ mermaid_params = ["-p", "puppeteer-config.json"]
 
 
 # -- Sphinx setup functions --------------------------------------------------
+
+
 def skip_member(
-    app: Sphinx, what: str, name: str, obj: PythonPythonMapper, skip: bool, options: List[str]
+    app: Sphinx,  # noqa: ARG001
+    what: str,
+    name: str,
+    obj: PythonPythonMapper,
+    skip: bool,
+    options: List[str],  # noqa: ARG001
 ) -> bool:
     """Skip certain members of the API.
 
@@ -205,22 +269,14 @@ def skip_member(
     Returns:
         A boolean indicating if the member should be skipped.
     """
-    _package_set = {"tm_devices", "commands", "components", "driver_mixins", "drivers", "helpers"}
-    _ = app
-    _ = name
-    _ = options
     if (
-        what == "module"  # pylint: disable=too-many-boolean-expressions
-        or (
-            what == "package" and obj.short_name not in _package_set  # pyright: ignore [reportUnknownMemberType]
-        )
+        what == "module"
         or f"{os.path.sep}commands{os.path.sep}" in obj.pathname
         or obj.pathname.endswith(f"{os.path.sep}commands")
         or ("_commands" in name and "ieee" not in name)
     ):
         skip = True
-
-    return skip or os.getenv("SKIP_TM_DEVICES_API_GENERATION") is not None
+    return skip or SKIP_API_GEN
 
 
 def setup(sphinx: Sphinx) -> None:
